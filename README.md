53186d9b1f63c4a965464dfd55f796788200d711
Shahriar-GitHub <shahriar@aol.com>
2022-06-11 10:24:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fppstd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fppstd)
---
+++ src/Table.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Charlier</td>
+            <td>Jones</td>
+          </tr>
+          <tr>
+            <td>Henry</td>
+            <td>Smith</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+  
+}
+export default Table
